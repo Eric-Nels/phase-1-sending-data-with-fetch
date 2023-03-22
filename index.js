de4ec39138bd7a0f6277fb79d068974def07194f
Eric Nelson <ernelson12o0@gmail.com>
2023-03-22 18:14:47 -0400

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(custName, custEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: custName,
+            email: custEmail,
+        }) 
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            const newId = object.id;
+            document.body.append(newId)
+        })
+        .catch(function (error) {
+            document.body.append(error.message);
+        })
+}
+
+
